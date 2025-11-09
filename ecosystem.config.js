bacc94dd5b3e5cd94b6caa69ce2971d7589398c2
sivakrishnavegi <sivakrishnavegi@yahoo.com>
2025-11-09 22:01:58 +0530

    added changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,15 @@
+
 module.exports = {
   apps: [
     {
-      name: "skoolelite-dashboard",
+      name: "express-app",
       script: "npm",
       args: "start",
-      cwd: "/home/ubuntu/my-smartgymapi",
-      interpreter: "none",
+      cwd: "/home/ubuntu/my-smartgymapi", // full path to your project
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "500M",
       env: {
         NODE_ENV: "production",
         PORT: 3000
