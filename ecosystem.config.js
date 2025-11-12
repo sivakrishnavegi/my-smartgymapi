8f3e0df062ad898750f715e3a1b294c25a21cdef
sivakrishnavegi <sivakrishnavegi@yahoo.com>
2025-11-12 22:15:31 +0530

    added changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,16 @@
-
 module.exports = {
   apps: [
     {
       name: "express-app",
-      script: "npm",
-      args: "start",
-      cwd: "/home/ubuntu/my-smartgymapi", // full path to your project
+      script: "node",
+      args: "dist/server.js",
+      cwd: "/home/ubuntu/my-smartgymapi",
       instances: 1,
+      exec_mode: "fork", // simpler for small servers
       autorestart: true,
       watch: false,
-      max_memory_restart: "500M",
+      node_args: "--max-old-space-size=4096", // allocate more memory
+      max_memory_restart: "1G",
       env: {
         NODE_ENV: "production",
         PORT: 3000
